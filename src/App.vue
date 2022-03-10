a4a26b2ae4cc65af80e75839c32120444abbfa7a
HYO00 <hyo02222@gmail.com>
2022-03-10 11:14:25 +0900

    study v-for & event handler

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  
+
   <div class="menu">
     <a v-for="작명 in subbar" :key="작명">{{작명}}</a>
   </div>
@@ -7,6 +7,8 @@
   <div v-for="(list, i) in 3" :key="i">
     <h4 :style="스타일">{{products[i]}}</h4>
     <p >{{price[i]}} 만원</p>
+    <button @click="increase(i)">상세보기</button>
+    <span>likes : {{like[i]}}</span>
   </div>
  
 </template>
@@ -21,12 +23,19 @@ export default {
   data(){
     return {
       //여기에 데이터 보관 변수같은거 데이터는 object 자료로 저장 {데이터이름: 데이터내용}
+      like:[0,0,0],
       price: [5,8,14],
-      products: ['파랑맨투맨', '노랑맨투맨', "빨간모자"],
+      products: ['자켓', '맨투맨', "후드집업"],
       subbar:["home","menu","about"],
       스타일: 'color: blue',
      
     }
+  },
+  methods:{
+    increase(i){
+			this.like[i]++
+      console.log("click")
+		}
   },
   components: {
   }
